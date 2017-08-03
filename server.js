e30b6e5cab35d763f75ec6ea5d89da3632a2c532
briankeane <lonesomewhistle@gmail.com>
2017-08-03 16:26:10 -0500

    createContact2

--- server.js
+++ server.js
@@ -11,9 +11,24 @@ app.use(morgan('dev'));
 app.use(bodyParser.json())
 
 app.post('/contacts', function (req, res) {
-  console.log(req);
-  res.send('ok');
-});
+  // grab the posted info
+  var contactInfo = req.body;
+
+  // add an id
+  contactInfo.id = contactID;
+
+  // increment the id for the next time
+  contactID++;
+
+  // add it to the in-memory array
+  contacts.push(contactInfo);
+
+  console.log(contacts);
+  
+  // tell the message sender that the contact has been added
+  res.status(201).send(contactInfo);
+}); 
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
