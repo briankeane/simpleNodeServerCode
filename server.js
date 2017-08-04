88d6aff13bfe9cc947f37780d46afe852289c4ff
briankeane <lonesomewhistle@gmail.com>
2017-08-04 15:11:37 -0500

    add search

--- server.js
+++ server.js
@@ -58,7 +58,35 @@ app.delete('/contacts/:id', function (req, res) {
 
 app.get('/contacts', function (req, res) {
   return res.status(200).send(contacts);
-})
+});
+
+app.get('/search', function (req, res) {
+  var nameToMatch = req.query.name;
+  var emailToMatch = req.query.email;
+
+  var results = [];
+
+  for (var i=0;i<contacts.length;i++) {
+    var matchesName = true;
+    var matchesEmail = true;
+
+    // IF a name was provided and it doesn't match...
+    if (nameToMatch && (contacts[i].name != nameToMatch)) {
+      matchesName = false;
+    }
+
+    // IF an email was provided and it doesn't match
+    if (emailToMatch && (contacts[i].email != emailToMatch)) {
+      matchesEmail = false;
+    }
+
+    // IF it passed both tests, include it in the results
+    if (matchesName && matchesEmail) {
+      results.push(contacts[i]);
+    }
+  }
+  return res.status(200).send({ results: results });
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
