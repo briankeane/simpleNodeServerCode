3d4f3e2f3b45ed9af39db9d09bb94c89a22d9378
briankeane <lonesomewhistle@gmail.com>
2017-08-04 15:41:25 -0500

    3-02

---
+++ contacts.js
@@ -0,0 +1,23 @@
+function ContactsHandler() {
+  var self = this;
+
+  this.contacts = [];
+
+  this.find = function (attrs, callback) {
+
+  };
+
+  this.findById = function (attrs, callback) {
+
+  };
+
+  this.Create = function (attrs, callback) {
+
+  };
+
+  this.remove = function (attrs, callback) {
+
+  };
+}
+
+module.exports = new ContactsHandler();
