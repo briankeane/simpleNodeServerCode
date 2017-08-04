275063118ecc88c2f3b0b2efc7a28fd6b064c017
briankeane <lonesomewhistle@gmail.com>
2017-08-04 16:23:44 -0500

    3-05b

--- contact.spec.js
+++ contact.spec.js
@@ -2,10 +2,11 @@ const expect = require('chai').expect;
 const Contact = require('./contact.js');
 
 describe('A Contact', function () {
-  it('creates a contact', function () {
+  it('creates a contact', function (done) {
     Contact.Create({ name: 'bob', email: 'bob#bob.com' }, function (err, createdContact) {
       expect(createdContact.name).to.equal('bob');
       expect(createdContact.email).to.equal('bob@bob.com');
+      done();
     });
   });
 });
