122d58353f3ec8db6e5dbb2513a6520fa9170237
briankeane <lonesomewhistle@gmail.com>
2017-08-05 10:10:15 -0500

    2-08

--- contact.spec.js
+++ contact.spec.js
@@ -10,4 +10,52 @@ describe('A Contact', function () {
       done();
     });
   });
+
+  describe('find', function () {
+    it.only ('finds by name', function (done) {
+      Contact.contacts = [
+                            {
+                              name: 'bob',
+                              email: 'bob@bob.com',
+                              id: 1
+                            },
+                            {
+                              name: 'sam',
+                              email: 'sam@sam.com',
+                              id: 2
+                            },
+                            {
+                              name: 'bill',
+                              email: 'bill@bill.com',
+                              id: 3
+                            },
+                            {
+                              name: 'bob',
+                              email: 'bob@bob.com',
+                              id: 4
+                            },
+                            {
+                              name: 'bill',
+                              email: 'bill@bill.com',
+                              id: 5
+                            }
+                          ];
+      Contact.find({ name: 'bob' }, function (err, foundContacts) {
+        expect(foundContacts.length).to.equal(2);
+        var ids = foundContacts.map((contact) => contact.id);
+        expect(ids).to.contain(1);
+        expect(ids).to.contain(4);
+
+        Contact.find({ name: 'sam' }, function (err, otherFoundContacts) {
+          expect(otherFoundContacts.length).to.equal(1);
+          expect(otherFoundContacts[0].id).to.equal(2);
+          done();
+        });
+      });
+    });
+
+    it ('finds by name', function () {
+
+    });
+  });
 });
