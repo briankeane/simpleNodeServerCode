69b0ec38dd42f857e0df5ecd4cec04e8016977d4
briankeane <lonesomewhistle@gmail.com>
2017-08-05 10:19:17 -0500

    3-09b

--- contact.js
+++ contact.js
@@ -5,7 +5,30 @@ function ContactsHandler() {
   this.contactsID = 1;
 
   this.find = function (attrs, callback) {
+    var nameToMatch = attrs.name;
+    var emailToMatch = attrs.email;
+    var results = [];
 
+    for (var i=0;i<self.contacts.length;i++) {
+      var matchesName = true;
+      var matchesEmail = true;
+
+      // IF a name was provided and it doesn't match...
+      if (nameToMatch && (self.contacts[i].name != nameToMatch)) {
+        matchesName = false;
+      }
+
+      // IF an email was provided and it doesn't match
+      if (emailToMatch && (self.contacts[i].email != emailToMatch)) {
+        matchesEmail = false;
+      }
+
+      // IF it passed both tests, include it in the results
+      if (matchesName && matchesEmail) {
+        results.push(self.contacts[i]);
+      }
+    }
+    callback(null, results);
   };
 
   this.findById = function (attrs, callback) {
