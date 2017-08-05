c37d22f8d5be27f62949ebd7c7b357ed781f6266
briankeane <lonesomewhistle@gmail.com>
2017-08-05 17:44:11 -0500

    3-21b

--- server/contact/contact.routes.js
+++ server/contact/contact.routes.js
@@ -1,55 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const Contact = require('./contact.js')
+const controller = require('./contact.controller.js');
+// POST
+router.post('/', controller.create); 
 
+// GET
+router.get('/search', controller.search);
+router.get('/:id', controller.show);
+router.get('/', controller.list);
 
-router.post('/', function (req, res) {
-  Contact.Create(req.body, function (err, createdContact) {
-    res.status(201).send(createdContact);
-  });
-}); 
+// PUT
+router.put('/:id', controller.modify);
 
-router.get('/search', function (req, res) {
-  console.log('here');
-  Contact.find(req.query, function (err, results) {
-    return res.status(200).send({ results: results });
-  });
-});
-
-router.get('/:id', function (req, res) {
-  Contact.findById(Number(req.params.id), function (err, foundContact) {
-    if (foundContact) {
-      return res.status(200).send(foundContact);
-    }
-    return res.status(404).send({ message: 'contact not found'});
-  });
-});
-
-router.put('/:id', function (req, res) {
-  Contact.findById(Number(req.params.id), function (err, foundContact) {
-    if (foundContact) {
-      foundContact = Object.assign(foundContact, req.body);
-      return res.status(200).send(foundContact);
-    }
-    return res.status(404).send({ message: 'contact not found'});
-  });
-});
-
-router.delete('/:id', function (req, res) {
-  Contact.findById(Number(req.params.id), function (err, foundContact) {
-    if (!foundContact) {
-      return res.status(404).send({ message: 'contact not found' });
-    }
-    Contact.remove({ id: Number(req.params.id) }, function (err) {
-      return res.status(200).send({ message: 'contact deleted' });
-    });
-  });
-});
-
-router.get('/', function (req, res) {
-  Contact.find({}, function (err, foundContacts) {
-    return res.status(200).send(foundContacts);
-  });
-});
+// DELETE
+router.delete('/:id', controller.delete);
 
 module.exports = router;
